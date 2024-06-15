7cc8a0fffe206a8650a3bec18121e33c16403d39
Bakhtiyar Tukhtasinov <144336544+tuxtasinow@users.noreply.github.com>
2024-06-15 23:02:23 +0500

    Add files via upload

---
+++ lesson4(13.06.2024).py
@@ -0,0 +1,8 @@
+my_string = input('Привет, я считаю количество символов в строке. Введите строку: ')
+print(len(my_string))  #Длина строки
+
+print(my_string.upper())  #Верхний регистр
+print(my_string.lower())  #Нижний регистр
+print(my_string.replace(' ', ''))  #Удаление пробелов
+print(my_string[0])  #Первый символ
+print(my_string[-1])  #Последний символ
